d797048e0da0db55d7afeeed8284633e9f85d965
Anshay Agarwal <anshayagr@gmail.com>
2019-05-16 22:25:55 +0530

    Adds Convolutional Layers
    Adds 2 CNN layers along with 2 MaxPooling layers to the network containing 64 (3, 3) filters.
    Also reshapes training ang test data to be compatible to Conv2D input shape requirements.

--- 5_fashion_mnist/main.py
+++ 5_fashion_mnist/main.py
@@ -12,17 +12,23 @@ plt.show()
 ## normalizing data
 train_data = train_data / 255.0
 test_data = test_data / 255.0
+train_data = train_data.reshape(60000, 28, 28, 1)
+test_data = test_data.reshape(10000, 28, 28, 1)
 
 ## define a callback
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if (logs.get("loss") < 0.4):
+        if (logs.get("loss") < 0.1):
             print ("\nStopping training on reaching 0.4 loss")
             self.model.stop_training = True
 callback = myCallback()
 
 ## define network model
-model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
+model = tf.keras.Sequential([tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+                             tf.keras.layers.MaxPooling2D(2, 2),
+                             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                             tf.keras.layers.MaxPooling2D(2, 2),
+                             tf.keras.layers.Flatten(),
                              tf.keras.layers.Dense(512, activation = tf.nn.relu),
                              tf.keras.layers.Dense(10, activation = tf.nn.softmax)])
 model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
