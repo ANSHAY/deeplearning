898da9f1fb137d0ebefde8c556ca2dfb9a94c46a
Anshay <anshayagr@gmail.com>
2019-05-29 07:53:52 -0700

    Create main.py
    Creates frame for cats vs dogs classification for kaggle competition on Microsoft cat_dog dataset

---
+++ 8_cats_vs_dogs/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+## import modules
+
+## load data
+
+## visualize data
+
+## preprocess data
+
+## define model
+
+## fit model to data - training
+
+## test model on new data - testing
