7e625a98c1c3dc33601b81f7ada12137ed2e4712
Anshay <anshayagr@gmail.com>
2019-06-04 17:27:10 +0530

    Added Classification of anime vs real images
    This classifies between anime images and real photographs. The dataset contains variety of images collected from internet of real photographs and anime/digitally drawn images. This builds a classifier for the two using CNN and tensorflow.

---
+++ 10_anime_vs_real/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# Filename: main.py
+"""
+Created on Thu May 30 00:23:55 2019
+
+@author: Anshay
+Main file for canime vs real classification
+
+Runs anime vs real classification on selected dataset.
+Loads data, visualizes data, fetch/train model, saves
+model and evaluates the model.
+"""
+
+## import modules
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import model_gen
+
+## define metadata
+print("\nDefining metadata")
+metadata = {'Nrows':300, 'Ncols':300, 'BATCH_SIZE':64, 'NUM_EPOCHS':30,
+            'FILTER_SIZE':(3,3)}
+print("\nMetadata defined")
+
+## load data - change directories to the location of data
+print("\nLoading data...")
+train_dir = "D:\\Datasets\\anime_vs_real\\train\\"
+val_dir = "D:\\Datasets\\anime_vs_real\\val\\"
+
+data_gen = ImageDataGenerator(rescale=1./255,
+                              horizontal_flip=True,
+                              rotation_range=40,
+                              width_shift_range=10,
+                              height_shift_range=10,
+                              zoom_range=2,
+                              shear_range=40
+                              )
+
+train_gen = data_gen.flow_from_directory(
+                    train_dir,
+                    target_size=((metadata['Nrows'], metadata['Ncols'])),
+                    batch_size=metadata['BATCH_SIZE'],
+                    class_mode='binary')
+val_gen = data_gen.flow_from_directory(
+                    val_dir,
+                    target_size=((metadata['Nrows'], metadata['Ncols'])),
+                    batch_size=metadata['BATCH_SIZE'],
+                    class_mode='binary')
+print("\nData Generators defined")
+
+## visualize data
+
+## preprocess data
+
+## fetch model (training)
+print("\nTraining model...")
+model = model_gen.fetch_model(train_gen, val_gen, metadata)
+print("\nTraining Complete")
---
+++ 10_anime_vs_real/model_gen.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# Filename: model_gen.py
+
+## import modules
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+import plot_results as pltres
+
+## define callbacks
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epochs, logs={}):
+        if (logs.get('acc') > 0.99):
+            self.model.stop_training = True
+            print ("\nStopping training as accuracy is above 99%")
+callback = myCallback()
+
+## define model
+def fetch_model(train_gen, val_gen, metadata):
+    model_path = "models\\model" +\
+                 "_R"+str(metadata['Nrows']) +\
+                 "_C"+str(metadata['Ncols']) +\
+                 "_fs"+str(metadata['FILTER_SIZE'][0]) +\
+                 "_ep"+str(metadata['NUM_EPOCHS']) +\
+                 ".h5"
+    try:
+        print("\nLoading saved model")
+        model = tf.keras.models.load_model(model_path)
+        print("\nmodel loaded")
+    except:
+        print("\nModel not found. Training new model...")
+        model = tf.keras.Sequential([tf.keras.layers.Conv2D(16, metadata['FILTER_SIZE'], activation='relu', input_shape=(metadata['Nrows'],metadata['Ncols'],3)),
+                                     tf.keras.layers.MaxPooling2D(2,2),
+                                     tf.keras.layers.Conv2D(32, metadata['FILTER_SIZE'], activation='relu'),
+                                     tf.keras.layers.MaxPooling2D(2,2),
+                                     tf.keras.layers.Conv2D(64, metadata['FILTER_SIZE'], activation='relu'),
+                                     tf.keras.layers.MaxPooling2D(2,2),
+                                     tf.keras.layers.Conv2D(64, metadata['FILTER_SIZE'], activation='relu'),
+                                     tf.keras.layers.MaxPooling2D(2,2),
+                                     tf.keras.layers.Flatten(),
+                                     tf.keras.layers.Dense(512, activation='relu'),
+                                     tf.keras.layers.Dense(1, activation='sigmoid')])
+        ## compile model
+        model.compile(optimizer=RMSprop(lr=0.001),
+                      loss='binary_crossentropy',
+                      metrics=['acc'])
+        model.summary()
+        ## fit model to data - training
+        history = model.fit_generator(train_gen,
+                                      epochs=metadata['NUM_EPOCHS'],
+                                      validation_data=val_gen,
+                                      verbose=1,
+                                      callbacks=[callback])
+        print("\nNew model trained")
+        ## save model to file
+        print("\nSaving model for later use...")
+        model.save(model_path)
+        print("\nModel Successfully saved")
+        ## plot results
+        print("\nPlotting results...")
+        pltres.plot_results(history)
+        print("\n........................")
+    return model
