83533409a964faec3e8e19fe6fd9fc1bb8c9dcca
Anshay <anshayagr@gmail.com>
2019-07-17 14:26:19 +0530

    added training data. cleanup.

--- 17_chatterbox/cleandata.py
+++ 17_chatterbox/cleandata.py
@@ -3,7 +3,7 @@ import os
 #data_path = 'D:\\Datasets\\chatterbot-corpus-master\\chatterbot_corpus\\data\\english\\'
 #files = os.listdir(data_path)
 data_path = ''
-files = ['train2.txt']
+files = ['conversation_train3.txt']
 
 corpus = ''
 
@@ -13,11 +13,29 @@ for file in files:
         corpus += data
         f.close()
 
+corpus = corpus.replace('            Search Images      Translate','')
+corpus = corpus.replace('1\n Repeat','')
+corpus = corpus.replace('2\n Repeat','')
+corpus = corpus.replace('3\n Repeat','')
+corpus = corpus.replace('4\n Repeat','')
+corpus = corpus.replace('5\n Repeat','')
+corpus = corpus.replace('6\n Repeat','')
+corpus = corpus.replace('Practice the Conversations of This Topic with Mike','')
+corpus = corpus.replace('\nBack\n','')
+corpus = corpus.replace('Copyright © 2019. All rights reserved.','')
+corpus = corpus.replace('eslfast.com','')
+corpus = corpus.replace('? ','\n')
+corpus = corpus.replace('. ','\n')
+corpus = corpus.replace(', ','\n')
+corpus = corpus.replace('! ','\n')
 corpus = corpus.replace('\n\n','\n')
+corpus = corpus.replace('\n\n','\n')
+corpus = corpus.replace('\n\n','\n')
+
 corpus = corpus.replace('A: ','')
 corpus = corpus.replace('B: ','')
 
-with open('conversation_train2.txt', 'w') as f:
+with open('conversation_train3.txt', 'w') as f:
     f.write(corpus)
     f.close()
 print('done')
